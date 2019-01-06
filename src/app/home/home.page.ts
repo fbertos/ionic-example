5d4b4ee8e95e7cf6f726fe758bfafc436d66b9b0
Fernando Bertos Matilla <fbertos@gmail.com>
2019-01-06 15:50:38 +0100

    Version 1.0

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
 
+import { Geolocation } from '@ionic-native/geolocation/ngx';
+
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+   lat : any;
+   lng : any;
 
+  constructor(public geolocation : Geolocation) {
+  }
+
+  ngOnInit() {
+     this.geolocation.getCurrentPosition().then((resp) => {
+      this.lat = resp.coords.latitude;
+      this.lng = resp.coords.longitude;
+      console.log('OK');
+     }).catch((error) => {
+       console.log('Error getting location', error);
+     });
+  }
 }
